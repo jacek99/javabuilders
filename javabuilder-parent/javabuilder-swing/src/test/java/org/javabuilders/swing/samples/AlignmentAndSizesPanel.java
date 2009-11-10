64d70c0bac72fbba9823d21a4289adeb19701785
Jacek Furmankiewicz <jacek.furmankiewicz@radialpoint.com>
2009-11-10 11:25:48 -0500

    Issue 70: Fixed max width/height to use growx/growy instead, MigLayout was resizing them in an unexpected way otherwise

---
+++ javabuilder-parent/javabuilder-swing/src/test/java/org/javabuilders/swing/samples/AlignmentAndSizesPanel.java
@@ -0,0 +1,12 @@
+package org.javabuilders.swing.samples;
+
+import org.javabuilders.swing.SwingJavaBuilder;
+
+public class AlignmentAndSizesPanel extends SamplePanel {
+
+	public AlignmentAndSizesPanel() throws Exception {
+		super();
+		SwingJavaBuilder.build(this);
+	}
+
+}
